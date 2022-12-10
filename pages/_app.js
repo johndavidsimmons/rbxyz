aad9d734c95da4f16c38d851c610acb7e9bd2fe8
jsproton <johnsimmons@protonmail.com>
2022-12-10 00:28:45 -0500

    local serve twitter

--- pages/_app.js
+++ pages/_app.js
@@ -32,7 +32,7 @@ theme.typography.h2 = {
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Partytown debug={process.env.PARTYTOWN_DEBUG == 'true' ? true : false} forward={['dataLayer.push']} />
+      <Partytown debug={process.env.PARTYTOWN_DEBUG == 'true' ? true : false} forward={['dataLayer.push']} loadScriptsOnMainThread={['https://www.youtube.com/iframe_api']} />
       <Script
         id='gtm'
         dangerouslySetInnerHTML={{
@@ -44,7 +44,7 @@ function MyApp({ Component, pageProps }) {
       id="twitterpixel"
       dangerouslySetInnerHTML={{
         __html:`!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-        },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+        },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='uwt.js',
         a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
         twq('config','od5ij');`
       }}
